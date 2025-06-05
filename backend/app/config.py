c735d0f60da5d3926181845aba1c6930c543bec2
Diego Bruno <dmbruno61@gmail.com>
2025-06-05 12:32:40 -0300

    se migro las base a postgress

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,5 +4,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, '..', 'instance', 'portfolio.db')
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        f"sqlite:///{DB_PATH}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
